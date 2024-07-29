05b800415d918d14818799317f6b177a19ddb31d
lucasbizachi-dev <lucasbizachi.dev@gmail.com>
2024-07-29 14:24:15 -0300

    Adiciona funcionalidade de entrada contínua de itens na lista
    - Implementa um loop para solicitar ao usuário a entrada de itens a serem adicionados ao arquivo 'lista.txt'
    - Permite ao usuário continuar adicionando itens até que 'sair' seja digitado
    - Adiciona cada item digitado pelo usuário ao arquivo 'lista.txt' em modo de append

--- write.rb
+++ write.rb
@@ -1,4 +1,9 @@
-File.open('lista.txt', 'a') do |line|
-    line.puts('maionese')
-    line.puts('azeitona')
-end
+File.open('lista.txt', 'a') do |file|
+    loop do
+      puts "Digite um item para adicionar à lista (ou digite 'sair' para terminar):"
+      item = gets.chomp
+      break if item.downcase == 'sair'
+      file.puts(item)
+    end
+  end
+  
